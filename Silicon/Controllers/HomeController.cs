1063fec0e7a25ab27a4d7d0ea0db4a61d0422a01
Albulena <AlbulenaShabani/html-css.git>
2024-03-08 14:24:10 +0100

    Add project files.

---
+++ Silicon/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewData["Title"] = "Home";
+        return View();
+    }
+}
