aa12898837bfcc1fb84cf6c96eda55460dbbc79f
Albulena <AlbulenaShabani/html-css.git>
2024-03-11 12:31:35 +0100

    Accountdetails html and scss init

---
+++ Silicon/Controllers/AccountDetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon.Controllers
+{
+	public class AccountDetailsController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
