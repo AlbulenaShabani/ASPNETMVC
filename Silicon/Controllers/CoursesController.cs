5e338c4f8ea59452af7e5ba11bf034adf5897ba3
Albulena <AlbulenaShabani/html-css.git>
2024-03-11 11:30:31 +0100

    Courses finished

---
+++ Silicon/Controllers/CoursesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
